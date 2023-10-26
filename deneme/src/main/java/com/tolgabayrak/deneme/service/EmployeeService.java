9d131e615a854ed2578b2ce80e833d8052547437
tolgabayrakdev <bayraktolga28@gmail.com>
2023-10-27 00:21:08 +0300

    spirng security

--- deneme/src/main/java/com/tolgabayrak/deneme/service/EmployeeService.java
+++ deneme/src/main/java/com/tolgabayrak/deneme/service/EmployeeService.java
@@ -3,10 +3,10 @@ package com.tolgabayrak.deneme.service;
 import com.tolgabayrak.deneme.dto.EmployeeDto;
 import com.tolgabayrak.deneme.model.Employee;
 import com.tolgabayrak.deneme.repository.EmployeeRepository;
+import com.tolgabayrak.deneme.util.Helper;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -17,9 +17,12 @@ public class EmployeeService {
 
     private final ModelMapper modelMapper;
 
-    public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
+    private final Helper helper;
+
+    public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper, Helper helper) {
         this.employeeRepository = employeeRepository;
         this.modelMapper = modelMapper;
+        this.helper = helper;
     }
 
     public Employee create(Employee employeeData){
@@ -31,7 +34,6 @@ public class EmployeeService {
         Optional<Employee> employee = employeeRepository.findById(id);
         if (employee.isPresent()){
             return modelMapper.map(employee, EmployeeDto.class);
-
         }else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee not found");
         }
--- deneme/src/main/java/com/tolgabayrak/deneme/util/Helper.java
+++ deneme/src/main/java/com/tolgabayrak/deneme/util/Helper.java
@@ -1,43 +1,44 @@
 package com.tolgabayrak.deneme.util;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-import java.security.interfaces.RSAKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 
+import java.util.Date;
+
+@Component
 public class Helper {
 
-    private RSAPublicKey rsaPublicKey;
-    private RSAPrivateKey rsaPrivateKey;
+    private static final long accessTokenValidityMilliseconds = 15 * 60 * 1000;
 
+    private static final long refreshTokenValidityMilliseconds = 30L * 24 * 60 * 60 * 1000;
 
-    public String generateAccessToken(Object payload) {
-        try {
-            Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
-            String token = JWT.create()
-                    .withIssuer("auth0")
-                    .withClaim("key1", "value1") // Özelleştirilmiş payload verilerini ekleyebilirsiniz.
-                    .sign(algorithm);
-            return token;
-        } catch (JWTCreationException exception) {
-            throw new RuntimeException(exception);
-        }
+    public String generateAccessToken(String subject, Long id) {
+        long now = System.currentTimeMillis();
+        Date expirationDate = new Date(now + accessTokenValidityMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .claim("name", "Tolga")
+                .claim("id", id)
+                .setIssuedAt(new Date(now))
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256, "secret_key".getBytes())
+                .compact();
     }
 
-    public String generateRefreshToken(Object payload) {
-        try {
-            // Burada farklı bir algoritma kullanabilirsiniz, örnek olarak HS256:
-            Algorithm algorithm = Algorithm.HMAC256("your-secret-refresh-token-key");
-            String refreshToken = JWT.create()
-                    .withIssuer("auth0")
-                    .withClaim("key2", "value2") // Özelleştirilmiş payload verilerini ekleyebilirsiniz.
-                    .sign(algorithm);
-            return refreshToken;
-        } catch (JWTCreationException exception) {
-            throw new RuntimeException(exception);
-        }
+    public String generateRefreshToken(String subject) {
+        long now = System.currentTimeMillis();
+        Date expirationDate = new Date(now + refreshTokenValidityMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date(now))
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256,"secret_key".getBytes())
+                .compact();
     }
+
 }
